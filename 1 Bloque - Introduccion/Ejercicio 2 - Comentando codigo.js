57d79fe522f987eb75aded8a36d19cb70e77c471
User-Metro <86981227+User-Metro@users.noreply.github.com>
2021-10-19 15:55:39 -0500

    Add files via upload

---
+++ 1 Bloque - Introduccion/Ejercicio 2 - Comentando codigo.js
@@ -0,0 +1,12 @@
+//this is the output of a string
+console.log("Hello World!");
+
+//this is the output of a number
+console.log(22);
+
+//use: // to comment one line
+
+/*
+use: / * * / to comment a lot of lines
+of text. We mark the begin of our comment with /* and finished our comment with */
+
